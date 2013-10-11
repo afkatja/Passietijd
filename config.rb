ddac4a34693efdbd254fc795a98053676507f843
Katja Hollaar <katja@q42.nl>
2013-10-11 17:55:48 +0200

    yo

---
+++ config.rb
@@ -0,0 +1,32 @@
+# Require any additional compass plugins here.
+# require 'ruby-growl'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compact 
+# on_sprite_saved do |filename|
+ 
+#   g = Growl.new "localhost", "ruby-growl",
+#     ["ruby-growl Notification"]
+#   g.notify "ruby-growl Notification", "Compass sprite generation",
+#     "Your sprite is done:" + filename 
+ 
+# end
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
